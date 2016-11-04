ad4cda35c2e0a9224e613ac2cbfa13d6f91f5d5d
wami <wamiwami@live.jp>
2016-11-04 10:23:29 +0900

    サンプルプログラムを追加

---
+++ examples/easyEditersimple/easyEditersimple.ino
@@ -0,0 +1,18 @@
+#include<easyEditer.h>
+easyEditer editer;
+
+/*
+   easyEditerを試してみるサンプルプログラム
+   2016/10/20
+*/
+
+void setup() {
+  editer.begin("NefryEditer");//easyEditerを使えるようにします。
+}
+
+void loop() {
+  if (Nefry.push_SW()) {
+    //NefryのSWを押したときにeasyEditerを実行します。
+    editer.setTrigger();//easyEditerを使えるようにします。
+  }
+}
